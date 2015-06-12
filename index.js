190bb80369abecc6d6179038e7fdb2581870a1dd
Simon Schick <demwizzy@gmail.com>
2015-06-12 11:47:23 +0200

    simple server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+ 	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+  res.end('Hello World\n');
+}).listen(80, '0.0.0.0');
