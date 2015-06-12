9632834ad955065898bacb467b64b2d1bf99f230
Simon Schick <demwizzy@gmail.com>
2015-06-12 11:39:34 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var Q = require('q');
+
+
+function promisifyStream(stream) {
+	return new Q.Promise(function(resolve, reject) {
+		stream
+		.on('finish', resolve)
+		.on('error', reject);
+	});
+}
+
+function coveralls() {
+	return promisifyStream(
+		gulp.src(['coverage/lcov.info'])
+		.pipe($.coveralls())
+	);
+}
+
+function testAndCoverage(lcovOnly) {
+	var reportOptions;
+
+	if (lcovOnly) {
+		reportOptions = {
+			reporters: ['lcov', 'text', 'text-summary']
+		};
+	}
+
+	return promisifyStream(
+		gulp.src(['test/*.js'])
+		.pipe($.mocha())
+		.pipe($.istanbul.writeReports())
+		.pipe($.istanbul.enforceThresholds({
+			thresholds: {
+				global: 90
+			}
+		}))
+	);
+}
+
+function validateFiles(files, lcovOnly) {
+	return promisifyStream(
+		gulp.src(files)
+		.pipe($.jshint())
+		.pipe($.jshint.reporter(require('jshint-stylish')))
+		.pipe($.jshint.reporter('fail'))
+		.pipe($.jscs())
+		.pipe($.filter(['*', '!test/*']))
+		.pipe($.istanbul())
+		.pipe($.istanbul.hookRequire())
+	)
+	.then(function() {
+		return testAndCoverage(lcovOnly);
+	});
+}
+
+var files = [
+	'**/*.js',
+	'!node_modules/**/*',
+	'!docs/**/*',
+	'!coverage/**/'
+];
+
+gulp.task('default', function() {
+	return validateFiles(files);
+});
+
+gulp.task('travis', function() {
+	return validateFiles(files, true)
+	.then(coveralls);
+});
+
+gulp.task('git-pre-commit', ['default']);
---
+++ readme.md
@@ -0,0 +1,8 @@
+# heroku-test
+
+[![NPM](https://nodei.co/npm/heroku-test.png)](https://nodei.co/npm/heroku-test/)
+
+[![Build Status](https://travis-ci.org/SimonSchick/heroku-test.svg?branch=master)](https://travis-ci.org/SimonSchick/heroku-test)
+[![Dependencies](https://david-dm.org/SimonSchick/heroku-test.svg)](https://david-dm.org/SimonSchick/heroku-test)
+[![Coverage Status](https://coveralls.io/repos/SimonSchick/heroku-test/badge.svg)](https://coveralls.io/r/SimonSchick/heroku-test)
+[![npm version](http://img.shields.io/npm/v/heroku-test.svg)](https://npmjs.org/package/heroku-test)
